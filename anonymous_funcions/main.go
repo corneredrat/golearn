a56b0c29aec8c400d56c061c11033712860bc779
corneredrat <raghunandankst@gmail.com>
2021-11-21 13:05:44 +0530

    anonymous functions

--- anonymous_funcions/main.go
+++ anonymous_funcions/main.go
@@ -9,8 +9,8 @@ func main() {
 	moviesILike = []string{"Ulidavaru Kandanthe", "Kingdom of the Crystal Skull", "The Guest", "Shawshanl Redemption", "Shutter Island"}
 	allTimeFavouriteMovie = "Raiders of the lost Ark"
 	for _, movie := range moviesILike {
-		func(myFavouriteMovie string) {
-			fmt.Printf("%v is one of my favorite movies; %v is my all time favorite movie.\n", myFavouriteMovie, allTimeFavouriteMovie)
-		}(movie) // Actually this parameter doesn't even have to be passed; directly use `movie` in print invocation
+		func() {
+			fmt.Printf("%v is one of my favorite movies; %v is my all time favorite movie.\n", movie, allTimeFavouriteMovie)
+		}()
 	}
 }
